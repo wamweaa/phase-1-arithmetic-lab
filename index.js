9e6689dadff73dc949ec8b29c457e743f991dec2
wamwea <patokinya12@gmail.com>
2024-03-20 22:12:02 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+const num1 = 31
+const num2 = 2
+const multiply = num1*num2
+
+const random = Math.floor(Math.random() * 10) + 1
+
+const num3 = 22
+const num4 = 6
+const mod = num3%num4
+
+const max = Math.ceil(420/22)
